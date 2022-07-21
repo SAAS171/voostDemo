1352e1216527903b338a18425d220e245d7f668e
SAAS171 <53257963+SAAS171@users.noreply.github.com>
2022-07-21 19:41:22 +0500

    Updates

---
+++ precache-manifest.a1464c117f213540ef43d514aee4bdd1.js
@@ -0,0 +1,338 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7aa50cc52e7fbd5f599afbc9f6f12c75",
+    "url": "/voostDemo/index.html"
+  },
+  {
+    "revision": "90808867f6543dda289c",
+    "url": "/voostDemo/static/css/2.e1308751.chunk.css"
+  },
+  {
+    "revision": "c8c66ebe1c62f9b564bc",
+    "url": "/voostDemo/static/css/main.1fed2c52.chunk.css"
+  },
+  {
+    "revision": "90808867f6543dda289c",
+    "url": "/voostDemo/static/js/2.903a985c.chunk.js"
+  },
+  {
+    "revision": "108b190c6943544cb6c4065eb1755725",
+    "url": "/voostDemo/static/js/2.903a985c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8c66ebe1c62f9b564bc",
+    "url": "/voostDemo/static/js/main.dd62f09b.chunk.js"
+  },
+  {
+    "revision": "ec22463eb3ce33144ef5",
+    "url": "/voostDemo/static/js/runtime-main.13fc061c.js"
+  },
+  {
+    "revision": "8a490c32358161ed5e135c695e51cd49",
+    "url": "/voostDemo/static/media/BoldX.8a490c32.svg"
+  },
+  {
+    "revision": "d181a177ecade68999826a0a295a9fb4",
+    "url": "/voostDemo/static/media/MCC copy.d181a177.png"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/voostDemo/static/media/UploadIcon.1cedb6e9.svg"
+  },
+  {
+    "revision": "36e2a5b335668451d01349a52aeb7306",
+    "url": "/voostDemo/static/media/account-header.36e2a5b3.jpg"
+  },
+  {
+    "revision": "2001b95a55bcd84b343e0a6dae7b5b66",
+    "url": "/voostDemo/static/media/add.2001b95a.svg"
+  },
+  {
+    "revision": "e8e2b81e42f19bc71040d68bfb90c4b7",
+    "url": "/voostDemo/static/media/audio.e8e2b81e.svg"
+  },
+  {
+    "revision": "4526e3b68aa124788733374ab5b63f22",
+    "url": "/voostDemo/static/media/bell.4526e3b6.svg"
+  },
+  {
+    "revision": "3431fd28d28f7907f6589e2fe7004745",
+    "url": "/voostDemo/static/media/bolt.3431fd28.svg"
+  },
+  {
+    "revision": "2399cfb754bfcbf16270977926515d53",
+    "url": "/voostDemo/static/media/camera.2399cfb7.svg"
+  },
+  {
+    "revision": "3b0d49afdbedc5a483c831648d09ebda",
+    "url": "/voostDemo/static/media/chevron.3b0d49af.svg"
+  },
+  {
+    "revision": "0ceeb2e5a5f2b94b523380997f52ca4d",
+    "url": "/voostDemo/static/media/circle-tick.0ceeb2e5.svg"
+  },
+  {
+    "revision": "95434fa4057403767532b2142f517226",
+    "url": "/voostDemo/static/media/clock.95434fa4.svg"
+  },
+  {
+    "revision": "592a6da4965966c2e768fc7a5f98f3be",
+    "url": "/voostDemo/static/media/contact-header.592a6da4.jpg"
+  },
+  {
+    "revision": "f2b3e32ab9a246576c351667d5c5b656",
+    "url": "/voostDemo/static/media/create-cv.f2b3e32a.svg"
+  },
+  {
+    "revision": "5d27583ea225481eaa2944b6fbc57306",
+    "url": "/voostDemo/static/media/cross.5d27583e.svg"
+  },
+  {
+    "revision": "8d5532af017d5c4b0336bacb18e66de4",
+    "url": "/voostDemo/static/media/cvbuilderIcon.8d5532af.svg"
+  },
+  {
+    "revision": "2f3d8a14bb4ccde5c6490ae8e52f7bb6",
+    "url": "/voostDemo/static/media/document.2f3d8a14.svg"
+  },
+  {
+    "revision": "c77afdfe5df12ab299b1ad82b4e18be7",
+    "url": "/voostDemo/static/media/download.c77afdfe.svg"
+  },
+  {
+    "revision": "ae94846d1f6658011a180e55a43f9b9b",
+    "url": "/voostDemo/static/media/edit-field.ae94846d.svg"
+  },
+  {
+    "revision": "79e6f307ede502b74b01958218ec128c",
+    "url": "/voostDemo/static/media/edit-white.79e6f307.svg"
+  },
+  {
+    "revision": "ad5869e64b5bfa0522ce526c5ebfd007",
+    "url": "/voostDemo/static/media/ellipse-6.ad5869e6.svg"
+  },
+  {
+    "revision": "f7f153a73c6cd384350ec5f6bef231c4",
+    "url": "/voostDemo/static/media/email.f7f153a7.svg"
+  },
+  {
+    "revision": "1c31c6e7e260878bc238c19923aded03",
+    "url": "/voostDemo/static/media/exit.1c31c6e7.svg"
+  },
+  {
+    "revision": "d5306b7dfed5d3011ef63756efb2e348",
+    "url": "/voostDemo/static/media/features-1.d5306b7d.svg"
+  },
+  {
+    "revision": "8d18a18454ba2edb6448004f457c00e9",
+    "url": "/voostDemo/static/media/features-2.8d18a184.svg"
+  },
+  {
+    "revision": "1ab2eec8aa15dc00f986d62d4cc7f272",
+    "url": "/voostDemo/static/media/features-3.1ab2eec8.svg"
+  },
+  {
+    "revision": "d6829f1d36500c9673cbff1c51795f9f",
+    "url": "/voostDemo/static/media/features-4.d6829f1d.svg"
+  },
+  {
+    "revision": "8f82d7217d08aa94d7d5c016da0abd51",
+    "url": "/voostDemo/static/media/features-5.8f82d721.svg"
+  },
+  {
+    "revision": "b123deb5fb8aaba98404da8162ea41ba",
+    "url": "/voostDemo/static/media/features-6.b123deb5.svg"
+  },
+  {
+    "revision": "5f7c874dbb21ee0bd2f9f24e4d288368",
+    "url": "/voostDemo/static/media/features.5f7c874d.jpg"
+  },
+  {
+    "revision": "b797b72becc67619458122664ae4ab79",
+    "url": "/voostDemo/static/media/find.b797b72b.svg"
+  },
+  {
+    "revision": "0af29fab7c2f1f9026728d8cfe3b36c4",
+    "url": "/voostDemo/static/media/green-lightning.0af29fab.svg"
+  },
+  {
+    "revision": "c0efb090ef30acea4cbb4ff7dfd9956b",
+    "url": "/voostDemo/static/media/group.c0efb090.svg"
+  },
+  {
+    "revision": "99739c972e0ca3b2aabd78036579ab3c",
+    "url": "/voostDemo/static/media/hero.99739c97.jpg"
+  },
+  {
+    "revision": "c13bfa4f988d1257a6f124306ac9d36f",
+    "url": "/voostDemo/static/media/herosearch.c13bfa4f.svg"
+  },
+  {
+    "revision": "bfa99fc1d2e09f1bdf12bd1ea24bc6e8",
+    "url": "/voostDemo/static/media/hmrc.bfa99fc1.png"
+  },
+  {
+    "revision": "bbf639060ebc175fea350ada3d4a5f7b",
+    "url": "/voostDemo/static/media/init-loading.bbf63906.gif"
+  },
+  {
+    "revision": "ce6dbacc58ffa3602bd45d0927d6d114",
+    "url": "/voostDemo/static/media/jobs-header.ce6dbacc.jpg"
+  },
+  {
+    "revision": "eff4020381433d792325a04e4ab30478",
+    "url": "/voostDemo/static/media/lightning.eff40203.svg"
+  },
+  {
+    "revision": "6b9765800f09c6436e5004df913afe1d",
+    "url": "/voostDemo/static/media/location.6b976580.svg"
+  },
+  {
+    "revision": "c90dcac11409d1223bba4dff22231b62",
+    "url": "/voostDemo/static/media/logo.c90dcac1.svg"
+  },
+  {
+    "revision": "bf29a2a5f73628b510e2535b20bdb95d",
+    "url": "/voostDemo/static/media/logoWhite.bf29a2a5.svg"
+  },
+  {
+    "revision": "aeb69353542c278fe2dca326b6a7f1b2",
+    "url": "/voostDemo/static/media/mail.aeb69353.svg"
+  },
+  {
+    "revision": "808afd40f397de013675b3ca1c594cf6",
+    "url": "/voostDemo/static/media/mic.808afd40.svg"
+  },
+  {
+    "revision": "41ca2e4df9b9f16ec465dab8c7e692f4",
+    "url": "/voostDemo/static/media/minus.41ca2e4d.svg"
+  },
+  {
+    "revision": "d5152b4b9688aa7f5efab35f5edfeda3",
+    "url": "/voostDemo/static/media/modal-img.d5152b4b.png"
+  },
+  {
+    "revision": "c64ec1861c59f1595e6fca3b2f0d925e",
+    "url": "/voostDemo/static/media/mute.c64ec186.svg"
+  },
+  {
+    "revision": "c9f9ee297e4391c9bd58df93bf0bd0fc",
+    "url": "/voostDemo/static/media/natwest-logo-new.c9f9ee29.png"
+  },
+  {
+    "revision": "a2050abbc10430225cec1e60fd2af2e4",
+    "url": "/voostDemo/static/media/notification.a2050abb.mp3"
+  },
+  {
+    "revision": "6f1545b04a34863ee77fa7323de07dfb",
+    "url": "/voostDemo/static/media/participants.6f1545b0.svg"
+  },
+  {
+    "revision": "3262802cedfe9cc034bf1b63934bc946",
+    "url": "/voostDemo/static/media/phone.3262802c.svg"
+  },
+  {
+    "revision": "a4a03f65be7bf934802fb190b549c314",
+    "url": "/voostDemo/static/media/play.a4a03f65.svg"
+  },
+  {
+    "revision": "1cbf441263fd4217f63de5f176eba92f",
+    "url": "/voostDemo/static/media/plus.1cbf4412.svg"
+  },
+  {
+    "revision": "d9448eabedebb772ce0577b335ed7127",
+    "url": "/voostDemo/static/media/post-header.d9448eab.jpg"
+  },
+  {
+    "revision": "b6cc38a62bbd17933ccd088516c3b90b",
+    "url": "/voostDemo/static/media/postjobIcon.b6cc38a6.svg"
+  },
+  {
+    "revision": "6116819a771efaadedd9dc7606616f82",
+    "url": "/voostDemo/static/media/preview-img.6116819a.png"
+  },
+  {
+    "revision": "60c8f53b27592d100564813e132f25bc",
+    "url": "/voostDemo/static/media/profile.60c8f53b.svg"
+  },
+  {
+    "revision": "d594f832b009d9c04349e220b72a0536",
+    "url": "/voostDemo/static/media/profileRed.d594f832.svg"
+  },
+  {
+    "revision": "e7b3d49961e6f0469637826c75406184",
+    "url": "/voostDemo/static/media/search-tags.e7b3d499.svg"
+  },
+  {
+    "revision": "cf0d8a89502fef81842a0d132337fba4",
+    "url": "/voostDemo/static/media/select.cf0d8a89.svg"
+  },
+  {
+    "revision": "5efc3d032ca3fe20591d48b2e6077d23",
+    "url": "/voostDemo/static/media/send-note.5efc3d03.svg"
+  },
+  {
+    "revision": "0a290f980738125bc8f061c1c270af24",
+    "url": "/voostDemo/static/media/send.0a290f98.svg"
+  },
+  {
+    "revision": "2c7e14381857ed18b777da409f796a49",
+    "url": "/voostDemo/static/media/sent.2c7e1438.svg"
+  },
+  {
+    "revision": "232da815acda220777822e8299a026a5",
+    "url": "/voostDemo/static/media/sign-bg.232da815.svg"
+  },
+  {
+    "revision": "e78747d1f9cc1f3af7ad90a27bb02a49",
+    "url": "/voostDemo/static/media/sort-down.e78747d1.svg"
+  },
+  {
+    "revision": "4293c57966b65d8a462f93c2ce244866",
+    "url": "/voostDemo/static/media/sort-up.4293c579.svg"
+  },
+  {
+    "revision": "0b74c3d3ae987a4c46f14a844c5a8e15",
+    "url": "/voostDemo/static/media/star.0b74c3d3.svg"
+  },
+  {
+    "revision": "9317ce3f1c7d1041ddc323ad586547eb",
+    "url": "/voostDemo/static/media/timer.9317ce3f.svg"
+  },
+  {
+    "revision": "130e901aa70ce8ba29ce0e0ca48c4d67",
+    "url": "/voostDemo/static/media/trash.130e901a.svg"
+  },
+  {
+    "revision": "0363c8c1ebe8868fb16d0e182771e890",
+    "url": "/voostDemo/static/media/upload-cv.0363c8c1.svg"
+  },
+  {
+    "revision": "426c67521c18739fae77a0718f80e896",
+    "url": "/voostDemo/static/media/upload-file.426c6752.svg"
+  },
+  {
+    "revision": "95b6eeff42ff0c055aa923abc4d14c9d",
+    "url": "/voostDemo/static/media/upload.95b6eeff.svg"
+  },
+  {
+    "revision": "ec7f22da720c61e9c0abde5bd3ae7617",
+    "url": "/voostDemo/static/media/upload.ec7f22da.svg"
+  },
+  {
+    "revision": "5924354ab1b88f44b829b2499e3e558f",
+    "url": "/voostDemo/static/media/video.5924354a.svg"
+  },
+  {
+    "revision": "c8d0aef905e6aa073c1a9d63c54b58b3",
+    "url": "/voostDemo/static/media/voostroomIcon.c8d0aef9.svg"
+  },
+  {
+    "revision": "ff49ad35128c445fbec8ec0c835b7672",
+    "url": "/voostDemo/static/media/white-tick.ff49ad35.svg"
+  },
+  {
+    "revision": "9a8d768ec1996124a91a6efad58dd65b",
+    "url": "/voostDemo/static/media/x.9a8d768e.svg"
+  }
+]);
